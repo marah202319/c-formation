af33a4e7f5ba62790b78ac0c165d2f3fddecbd3b
marah202319 <marah202319@gmail.com>
2022-03-24 21:22:39 +0100

    test

---
+++ Serie 222/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_IV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("visaul studio ok github ok importation d'un fichier directement en le glissant ok push ok nickel ...");
+            // Keep the console window open
+            Console.WriteLine("----------------------");
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
