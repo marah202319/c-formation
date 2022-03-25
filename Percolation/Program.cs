e888a8751c3e6bb0a553b8d9a4642e2bdbb430fd
marah202319 <marah202319@gmail.com>
2022-03-25 16:58:49 +0100

    exo noté update

--- Percolation/Program.cs
+++ Percolation/Program.cs
@@ -10,8 +10,14 @@ namespace Percolation
     {
         static void Main(string[] args)
         {
+            /*3-b/  dans le pire des cas on va ouvrir une case qui un voisin plein et tt les autres cases ne sont pas plein.a ce moment on va les rendre pleines et la grille sera full.performance linéaire:O(n)
+             * 3-c/ parceque si on a une case pleine, le reste ne seront pas tous ouvert.
+             * 
+             */ 
             Percolation pp = new Percolation(10);
+            //pp.
             PercolationSimulation p = new PercolationSimulation();
+            p.PercolationValue(10);
             var p1=p.MeanPercolationValue(10,100);
             Console.WriteLine(p1.Mean +";" +p1.StandardDeviation +";" +p1.RelativeStd);
             Console.WriteLine("----------------------");
