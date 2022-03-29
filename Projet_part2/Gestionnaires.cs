416ee0b55a73e1a6fed52c8c134f77e854b958c5
marah202319 <marah202319@gmail.com>
2022-03-29 15:04:21 +0000

    debut gestionnaires

---
+++ Projet_part2/Gestionnaires.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projet_part2
+{
+	public class Gestionnaires
+	{
+		public int Id { get; set; }
+		public TypeGestionnaire Type;
+		public int Nbrtransactions;		
+		Dictionary<int, Compte> comptes;
+		
+		public Gestionnaires(int id, TypeGestionnaire type, int nbrtransactions)
+		{
+			Id = id;
+			Type = type;
+			Nbrtransactions = nbrtransactions;
+			comptes= = new Dictionary<int, Compte>();
+		}
+	}
+}
