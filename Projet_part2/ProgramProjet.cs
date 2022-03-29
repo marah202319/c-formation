cbaadcc08d16dde3a6858d3b4fe9659ffb094bb1
marah202319 <marah202319@gmail.com>
2022-03-29 15:13:37 +0000

    update

--- Projet_part2/ProgramProjet.cs
+++ Projet_part2/ProgramProjet.cs
@@ -12,6 +12,22 @@ namespace Projet_part2
     {
         static void Main(string[] args)
         {
+            /*
+            string path = Directory.GetCurrentDirectory();
+            // Fichiers entrée
+            string mngrPath = path + @"\Gestionnaires_1.txt";
+            string acctPath = path + @"\Comptes_1.txt";
+            string trxnPath = path + @"\Transactions_1.txt";
+            Fichiers sortie
+            string sttsAcctPath = path + @"\StatutOpe_1.txt";
+            string sttsTrxnPath = path + @"\StatutTra_1.txt";
+            string mtrlPath = path + @"\Metrologie_1.txt";
+
+            //TODO: Votre implémentation
+
+            // Keep the console window open
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();*/
             string path = Directory.GetCurrentDirectory();
             string acctPath = path + @"\Comptes_1.txt";
             string trxnPath = path + @"\Transactions_1.txt";
